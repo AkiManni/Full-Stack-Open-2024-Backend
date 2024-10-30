0e765bbdb84772712b0c6462e163e18b14b55361
Aki Manninen <t0maak00@students.oamk.fi>
2024-10-30 16:55:36 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan');
 const cors = require('cors')
 const app = express()
+const Contact = require('./models/contact')
 
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
+}
+
+// Gets rid of GET favicon.ico error msg on console
+//app.use('/favicon.ico', (req, res) => {
+//  res.status(204).end()
+//})
 
 morgan.token('req-body', (req) => {
   return JSON.stringify(req.body)
@@ -23,6 +43,8 @@ morgan.token('customStatus', (req, res) => {
   }
 })
 
+
+
 app.use((req, res, next) => {
   if (req.method === 'POST') {
     morgan(':method :url :customStatus :res[content-length] - :response-time ms :req-body')(req, res, next);
@@ -31,106 +53,109 @@ app.use((req, res, next) => {
   }
 });
 
-let persons = [  
-  {    
-    id: "1",    
-    name: "Arto Hellas",    
-    number: "040-123456" 
-  },  
-  {    
-    id: "2",    
-    name: "Ada Lovelace",    
-    number: "39-44-5323523"
-  },  
-  {    
-    id: "3",    
-    name: "Dan Abramov",    
-    number: "12-43-243535"  
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>\n<p>${Date()}</p>`)
+app.get('/info', async (request, response) => {
+  response.send(`<p>Phonebook has info for ${await Contact.countDocuments({})} people</p>\n<p>${Date()}</p>`)
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+  })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(person => person.id === id)
-  if (person) {    
-    response.json(person)  
-  } else {    
-    response.status(404).end()  
-  }
+app.get('/api/persons/:id', (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then(
+      contact => {
+        if(contact){
+          response.json(contact)
+        } else {
+          response.status(404).end()
+        }
+      })
+    .catch(error =>
+      next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
-  response.status(204).end()
+  Contact.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedContact => {
+      response.json(updatedContact)
+    })
+    .catch(error => next(error))
 })
 
-const generateId = () => {
-  const idList = persons.map(person => parseInt(person.id, 10))
-  let randomNumber;
+app.delete('/api/persons/:id', (request, response, next) => {
+  Contact.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  do{
-    randomNumber = Math.floor(Math.random() * 10001);
-  } while(idList.includes(randomNumber))
 
-  const newId = persons.length > 0
-    ? randomNumber
-    : 0;
-
-  return String(newId)
-}
-
-app.post('/api/persons', (request, response) => {  
+app.post('/api/persons', (request, response, next) => {  
   const body = request.body
 
-  if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
+  if (body.name === undefined) {
+    return response.status(400).json({ error: 'name missing' })
+  }
+  if (body.number === undefined) {
+    return response.status(400).json({ error: 'number missing' })
+  }
+
+  Contact.findOne({ name: body.name })
+    .then(existingContact => {
+      if (existingContact) {
+
+        /*  app.put(`/api/persons/${existingContact.id}`, request, response)
+          .then(savedContact => {
+            response.json(savedContact)
+          })
+          .catch(error => next(error)) */
+
+        // Task 3.17* requested using calling PUT method, so I did and the POST part worked,
+        // but when testing the later part 3.20 -
+        // I noticed that when I tried to send new modified number on Postman to a already existing one, it caused a crash.
+        // findByIdAndUpdate which was used in the examples, seemed to work as a roundabout for that.
+
+        Contact.findByIdAndUpdate(
+          existingContact.id,
+          { number: body.number },
+          { new: true }
+        )
+          .then(savedContact => {
+            response.json(savedContact)
+          })
+          .catch(error => next(error))
+
+      } else {
+        const contact = new Contact({
+          name: body.name,
+          number: body.number
+        })
+
+        contact
+          .save()
+          .then(savedContact => {
+            response.json(savedContact)
+          })
+          .catch(error => next(error))
+      }
     })
-  }
-
-  if (!body.number) {
-    return response.status(400).json({ 
-      error: 'number missing' 
-    })
-  }
-
-  if(persons.some(person => person.name.toLocaleLowerCase() == body.name.toLocaleLowerCase())){
-    return response.status(400).json({
-      error: 'name is already found, name must be unique'
-    })
-  }
-
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number || false
-  }
-
-  persons = persons.concat(person)
-
-  response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+// Must be put in use after every other middleware, OR ELSE
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
